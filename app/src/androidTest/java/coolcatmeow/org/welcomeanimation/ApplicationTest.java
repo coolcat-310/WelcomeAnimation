57457fbb2d26159984929f67e4de900f9f9facdf
juancarlosnavarrete <navarretejuan09@gmail.com>
2016-03-06 15:22:18 -0800

    Initial commit

---
+++ app/src/androidTest/java/coolcatmeow/org/welcomeanimation/ApplicationTest.java
@@ -0,0 +1,13 @@
+package coolcatmeow.org.welcomeanimation;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
