cdd80215a1fb0e8ec3f4b87c76f78e7af749c993
juancarlosnavarrete <navarretejuan09@gmail.com>
2016-04-29 14:25:13 -0700

    modify QR activity
    It was blank before, but now I added a text view and button. In
    QRActivity, there is a working button with a text field editor

--- app/src/main/java/coolcatmeow/org/welcomeanimation/QrActivity.java
+++ app/src/main/java/coolcatmeow/org/welcomeanimation/QrActivity.java
@@ -1,13 +1,16 @@
 package coolcatmeow.org.welcomeanimation;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class QrActivity extends AppCompatActivity {
+
+    public final static String SENDEMAIL = "coolcatmeow.org.welcomeanimation.SENDEMAIL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +19,20 @@ public class QrActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        Button goToMainActivity = (Button) findViewById(R.id.infoButton);
+            goToMainActivity.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    EditText fn = (EditText) findViewById(R.id.editText2);
+                    final String sendEmail = fn.getText().toString();
+
+                    Intent save = new Intent(QrActivity.this, MainActivity.class);
+                    save.putExtra(SENDEMAIL,sendEmail);
+                }
+            });
+
     }
 
 }
